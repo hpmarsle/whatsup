3bcb029399130f35bf92d3872485c0b3bf5fd4e7
Maria Le <hp.mars.le@gmail.com>
2020-09-08 14:52:58 -0500

    add login and create new id button, start handleSubmit eventhandler

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,16 @@
 import React, { useRef } from 'react'
-import { Container, Form } from 'react-bootstrap'
+import { Container, Form, Button } from 'react-bootstrap'
 
 export default function Login() {
     const idRef = useRef()
     
+    function handleSubmit(event) {
+        event.preventDefault()
+    }
+
     return (
-        <Container className='align-items-center d-flex'>
-            <Form>
+        <Container className='align-items-center d-flex' style={{ height: '100vh' }}>
+            <Form onSubmit={handleSubmit}>
                 <Form.Group>
                     <Form.Label>Enter Your Id</Form.Label>
                     <Form.Control type="text" ref={idRef} required />
@@ -15,6 +19,8 @@ export default function Login() {
                     <Form.Label>Enter Your Password</Form.Label>
                     <Form.Control type="password" />
                 </Form.Group>
+                <Button type='submit'>Login</Button>
+                <Button variant='secondary'>Create a New Id</Button>
             </Form>
         </Container>
     )
