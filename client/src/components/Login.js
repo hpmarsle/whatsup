eb33e891b7ffa05465217faa34f8aa5c9e11a963
Maria Le <hp.mars.le@gmail.com>
2020-09-08 16:15:59 -0500

    install uuid to generate random id when create new id button is clicked

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react'
 import { Container, Form, Button } from 'react-bootstrap'
+import { v4 as uuidV4 } from 'uuid'
 
 export default function Login({ onIdSubmit }) {
     const idRef = useRef()
@@ -9,6 +10,9 @@ export default function Login({ onIdSubmit }) {
         onIdSubmit(idRef.current.value)
     }
 
+    function createNewId() {
+        onIdSubmit(uuidV4())
+    }
     return (
         <Container className='align-items-center d-flex' style={{ height: '100vh' }}>
             <Form onSubmit={handleSubmit}>
@@ -21,7 +25,7 @@ export default function Login({ onIdSubmit }) {
                     <Form.Control type="password" />
                 </Form.Group>
                 <Button type='submit'>Login</Button>
-                <Button variant='secondary'>Create a New Id</Button>
+                <Button onClick={createNewId} variant='secondary'>Create a New Id</Button>
             </Form>
         </Container>
     )
