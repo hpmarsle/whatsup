9cbe4069ee8beae8fef62393aad96777c0c21639
Maria Le <hp.mars.le@gmail.com>
2020-09-08 02:51:34 -0500

    useRef hook

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,17 +1,19 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Container, Form } from 'react-bootstrap'
 
 export default function Login() {
+    const idRef = useRef()
+    
     return (
         <Container className='align-items-center d-flex'>
             <Form>
                 <Form.Group>
                     <Form.Label>Enter Your Id</Form.Label>
-                    <Form.Control type="text" ></Form.Control>
+                    <Form.Control type="text" ref={idRef} required />
                 </Form.Group>
                 <Form.Group>
                     <Form.Label>Enter Your Password</Form.Label>
-                    <Form.Control type="password" ></Form.Control>
+                    <Form.Control type="password" />
                 </Form.Group>
             </Form>
         </Container>
