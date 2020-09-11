f959e807c4283e864444573d795128f186720e42
Maria Le <hp.mars.le@gmail.com>
2020-09-11 18:47:16 -0500

    useState to set activeKey and start Tab.Content

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Tab, Nav } from 'react-bootstrap'
 
 const CONVERSATIONS_KEY = "conversations"
 const CONTACTS_KEY = "contacts"
 
 export default function Sidebar({id}) {
+    const [activeKey, setActiveKey] = useState(CONVERSATIONS_KEY)
+
     return (
-        <div style={{width: '250px'}} className='d-flex flex-column'>
-            <Tab.Container>
-                <Nav variant='tabs' className='justify-content-center'>
+        <div style={{width: '250px'}} className="d-flex flex-column">
+            <Tab.Container activeKey={activeKey} onSelect={setActiveKey}>
+                <Nav variant="tabs" className="justify-content-center">
                     <Nav.Item>
                         <Nav.Link eventKey={CONVERSATIONS_KEY} >Conversations</Nav.Link>
                     </Nav.Item>
@@ -16,8 +18,6 @@ export default function Sidebar({id}) {
                         <Nav.Link eventKey={CONTACTS_KEY} >Contacts</Nav.Link>
                     </Nav.Item>
                 </Nav>
-
-            Sidebar with {id}
             </Tab.Container>
         </div>
     )
