6903a7eb4b418d62073deb524a4339dca6cf0ef9
Maria Le <hp.mars.le@gmail.com>
2020-09-13 20:43:17 -0500

    add button to add contact or convo depending on pane rendered

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Tab, Nav } from 'react-bootstrap'
+import { Tab, Nav, Button } from 'react-bootstrap'
 import Conversations from './Conversations'
 import Contacts from './Contacts'
 
@@ -8,7 +8,8 @@ const CONTACTS_KEY = "contacts"
 
 export default function Sidebar({id}) {
     const [activeKey, setActiveKey] = useState(CONVERSATIONS_KEY)
-
+    const conversationsOpen = activeKey === CONVERSATIONS_KEY
+    
     return (
         <div style={{width: '250px'}} className="d-flex flex-column">
             <Tab.Container activeKey={activeKey} onSelect={setActiveKey}>
@@ -28,6 +29,12 @@ export default function Sidebar({id}) {
                         <Contacts />
                     </Tab.Pane>
                 </Tab.Content>
+                <div className="p-2 border-top border-right small">
+                    Your Id: <span className="text-muted">{id}</span>
+                </div>
+                <Button>
+                    New {conversationsOpen ? "Conversation" : "Contact" } 
+                </Button>
             </Tab.Container>
         </div>
     )
