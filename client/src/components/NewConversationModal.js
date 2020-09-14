f6272c359de911334b0c329e4b222abc28a545ae
Maria Le <hp.mars.le@gmail.com>
2020-09-14 05:29:48 -0500

    create forms for new contact and new convo modal, useRef for id and name fields

--- client/src/components/NewConversationModal.js
+++ client/src/components/NewConversationModal.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { Modal, Form, Button } from 'react-bootstrap'
 
 export default function NewConversationModal() {
+    const idRef = useRef()
+    const nameRef = useRef()
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
     return (
-        <div>
-            New Conversation Modal
-        </div>
+        <>
+            <Modal.Header closeButton>Create Conversation</Modal.Header>
+            <Modal.Body>
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group>
+                        <Form.Label>Id</Form.Label>
+                        <Form.Control type="text" ref={idRef} required />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control type="text" ref={nameRef} required />
+                    </Form.Group>
+                    <Button type="submit">Create</Button>
+                </Form>
+            </Modal.Body>
+        </>
     )
 }
