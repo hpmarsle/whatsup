887a2df915bea89a7cd2d7ee35d1be57c4ad3bb7
Maria Le <hp.mars.le@gmail.com>
2020-09-30 17:11:44 -0500

    call createContact in handleSubmit function

--- client/src/components/NewContactModal.js
+++ client/src/components/NewContactModal.js
@@ -10,7 +10,7 @@ export default function NewContactModal({closeModal}) {
     const handleSubmit = (e) => {
         e.preventDefault()
         
-        // createContact(idRef.current.value, nameRef.current.value)
+        createContact(idRef.current.value, nameRef.current.value)
         closeModal()
     }
 
