b603fd84fb8556d493060cc140c2b9e3d696fa06
Maria Le <hp.mars.le@gmail.com>
2020-09-30 18:22:37 -0500

    create Form Group and checkboxes for each available contact, define handleCheckboxChange and useState to store the selected contacts by id

--- client/src/components/NewConversationModal.js
+++ client/src/components/NewConversationModal.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Modal, Form, Button } from 'react-bootstrap'
 import { contacts } from '../contexts/ContactsProvider'
 
 export default function NewConversationModal({closeModal}) {
+    const [selectedContactIds, setSelectedContactIds] = useState([]) 
 
     const handleSubmit = (e) => {
         e.preventDefault()
@@ -10,12 +11,33 @@ export default function NewConversationModal({closeModal}) {
         closeModal()
     }
 
+    const handleCheckboxChange = (contactId) => {
+        setSelectedContactIds(prevSelectedContactIds => {
+            if (prevSelectedContactIds.includes(contactId)) {
+                return prevSelectedContactIds.filter(prevId => {
+                    return contactId !== prevId
+                })
+            } else {
+                return [...prevSelectedContactIds, contactId]
+            }
+        })
+    }
+
     return (
         <>
             <Modal.Header closeButton>Create Conversation</Modal.Header>
             <Modal.Body>
                 <Form onSubmit={handleSubmit}>
-                    
+                    {contacts.map(contact => (
+                        <Form.Group controlId={contact.id} key={contact.id}>
+                            <Form.Check
+                                type="checkbox"
+                                value={selectedContactIds.includes(contact.id)}
+                                label={contact.name}
+                                onChange={() => handleCheckboxChange(contact.id)}
+                            />
+                        </Form.Group>
+                    ))}
                     <Button type="submit">Create</Button>
                 </Form>
             </Modal.Body>
