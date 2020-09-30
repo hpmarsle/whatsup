a60807b0d2837711914668a7ed68f1b1240c7cd9
Maria Le <hp.mars.le@gmail.com>
2020-09-30 17:58:09 -0500

    edit boilerplate of New Conversation form

--- client/src/components/NewConversationModal.js
+++ client/src/components/NewConversationModal.js
@@ -1,9 +1,8 @@
-import React, { useRef } from 'react'
+import React from 'react'
 import { Modal, Form, Button } from 'react-bootstrap'
+import { contacts } from '../contexts/ContactsProvider'
 
 export default function NewConversationModal({closeModal}) {
-    const idRef = useRef()
-    const nameRef = useRef()
 
     const handleSubmit = (e) => {
         e.preventDefault()
@@ -16,14 +15,7 @@ export default function NewConversationModal({closeModal}) {
             <Modal.Header closeButton>Create Conversation</Modal.Header>
             <Modal.Body>
                 <Form onSubmit={handleSubmit}>
-                    <Form.Group>
-                        <Form.Label>Id</Form.Label>
-                        <Form.Control type="text" ref={idRef} required />
-                    </Form.Group>
-                    <Form.Group>
-                        <Form.Label>Name</Form.Label>
-                        <Form.Control type="text" ref={nameRef} required />
-                    </Form.Group>
+                    
                     <Button type="submit">Create</Button>
                 </Form>
             </Modal.Body>
