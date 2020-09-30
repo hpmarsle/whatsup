ab377c73495f95badd73f1a36948ff9792121386
Maria Le <hp.mars.le@gmail.com>
2020-09-30 17:08:47 -0500

    wrap dashboard in ContactsProvider from Contacts Context

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,13 +2,20 @@ import React from 'react';
 import Login from './Login';
 import Dashboard from './Dashboard'
 import useLocalStorage from '../hooks/useLocalStorage'
+import { ContactsProvider } from '../contexts/ContactsProvider';
 
 function App() {
-  
   const [id, setId] = useLocalStorage('id')
+
+  const dashboard = (
+    <ContactsProvider>
+      <Dashboard id={id} />
+    </ContactsProvider>
+  )
+
   return (
       <>
-        {id ? <Dashboard id={id}/> : <Login onIdSubmit={setId} />}
+        {id ? dashboard : <Login onIdSubmit={setId} />}
         
       </>
   );
