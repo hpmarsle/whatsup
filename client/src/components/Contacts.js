c18001fd3f096b823d8495cb42ee15adb755c82e
Maria Le <hp.mars.le@gmail.com>
2020-09-30 17:24:39 -0500

    render contacts from useContacts and map over each contact to display the contact name

--- client/src/components/Contacts.js
+++ client/src/components/Contacts.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import { ListGroup } from 'react-bootstrap'
 import { useContacts } from '../contexts/ContactsProvider'
 
 export default function Contacts() {
     const { contacts } = useContacts()
+    
     return (
-        <div>
-            {contacts}
-        </div>
+        <ListGroup variant="flush">
+            {contacts.map(contact => (
+                <ListGroup.Item key={contact.id} >
+                    {contact.name}
+                </ListGroup.Item>
+            ))}
+        </ListGroup>
     )
 }
