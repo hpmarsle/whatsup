fb3340b328fcd5185cd87b0bb78e4b977f0316c0
Maria Le <hp.mars.le@gmail.com>
2020-10-05 12:39:36 -0500

    create OpenConversation component to display selected conversation from Sidebar, nest as child component of Dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Sidebar from './Sidebar'
+import OpenConversation from "./OpenConversation";
 
 export default function Dashboard({id}) {
     return (
         <div className="d-flex" style={{ height: '100vh' }}>
             <Sidebar id={id} />
+            <OpenConversation />
         </div>
     )
 }
