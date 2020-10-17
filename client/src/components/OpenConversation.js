17e506d97c1ad5a5a06995f5dcbb0b0351095715
Maria Le <hp.mars.le@gmail.com>
2020-10-16 23:48:07 -0500

    import bootstrap form and input group and add form and starter styles

--- client/src/components/OpenConversation.js
+++ client/src/components/OpenConversation.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Form, InputGroup } from 'react-bootstrap'
 
 export default function OpenConversation() {
     return (
-        <div>
-            THIS WILL SHOW THE SELECTED CONVERSATION
+        <div className="d=flex flex-column flex-grow-1">
+            <div className="flex-grow-1 overflow-auto">
+                
+                THIS WILL SHOW THE SELECTED CONVERSATION
+            </div>
+            <Form>
+                <InputGroup>
+                    <Form.Control
+                        as="textarea"
+                        required
+                        value={text}
+                        onChange={e => setText(e.target.value)}
+                        style={{height: '75px', resize: 'none'}}
+                    />
+                </InputGroup>
+            </Form>
         </div>
     )
 }
