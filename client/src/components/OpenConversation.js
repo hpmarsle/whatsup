8c668857df322c87f6a9105b177ce715c8c7d91d
Maria Le <hp.mars.le@gmail.com>
2020-10-16 23:56:05 -0500

    correct typo d=flex to d-flex

--- client/src/components/OpenConversation.js
+++ client/src/components/OpenConversation.js
@@ -4,7 +4,7 @@ import { Form, InputGroup } from 'react-bootstrap'
 export default function OpenConversation() {
     const [text, setText] = useState("")
     return (
-        <div className="d=flex flex-column flex-grow-1">
+        <div className="d-flex flex-column flex-grow-1">
             <div className="flex-grow-1 overflow-auto">
                 
                 THIS WILL SHOW THE SELECTED CONVERSATION
