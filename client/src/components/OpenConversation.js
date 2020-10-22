f2b3d7ecc18b14536441575916a97041cd7de3b6
Maria Le <hp.mars.le@gmail.com>
2020-10-21 23:52:38 -0500

    create handleSubmit event handler for conversation form

--- client/src/components/OpenConversation.js
+++ client/src/components/OpenConversation.js
@@ -3,13 +3,20 @@ import { Form, InputGroup, Button } from 'react-bootstrap'
 
 export default function OpenConversation() {
     const [text, setText] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        sendMessage()
+        setText("")
+    }
+    
     return (
         <div className="d-flex flex-column flex-grow-1">
             <div className="flex-grow-1 overflow-auto">
                 
                 THIS WILL SHOW THE SELECTED CONVERSATION
             </div>
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Form.Group className="m-2">
                     <InputGroup>
                         <Form.Control
